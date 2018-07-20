e1683dd33bee31d6e3d0459659c594dab6a9011c
d27saurabh <d27saurabh@gmail.com>
2018-07-20 09:44:03 -0400

    create login test for an app(GoCanvas)

---
+++ AppiumBasics/src/xpathDetection.java
@@ -0,0 +1,30 @@
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+
+public class xpathDetection extends base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		
+		AndroidDriver<AndroidElement> driver = Capabilities();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+				
+		//Xpath Syntax //tagName[@attribute='value']
+		
+		driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textContains(\"Preference\").instance(0))");
+		driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
+		
+		driver.findElementById("android:id/checkbox").click();
+		driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+		driver.findElementByClassName("android.widget.EditText").sendKeys("Gocanvas WiFi");
+		driver.findElementsByClassName("android.widget.Button").get(1).click();
+				
+	}
+
+}
